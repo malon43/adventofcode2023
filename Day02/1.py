b9f166cc278c1a123ffdd16e92f0ad8d3c8e26bc
Jakub Maloštík <j.malostik@gmail.com>
2023-12-02 17:39:35 +0100

    Day 2

---
+++ Day02/1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from sys import stdin
+from collections import Counter
+
+
+class Choice:
+    def __init__(self, colors):
+        self.colors = colors
+
+    def possible(self, colors_max):
+        return all(count <= colors_max.get(color, 0) for color, count in self.colors.items())
+
+    @classmethod
+    def parse(cls, choice):
+        colors = Counter()
+        for count, color in map(lambda c: c.split(), choice.split(', ')):
+            colors[color] += int(count)
+        return cls(colors)
+
+
+class Game:
+    def __init__(self, id_, choices):
+        self.id = id_
+        self.choices = choices
+
+    def possible(self, colors_max):
+        return all(c.possible(colors_max) for c in self.choices)
+
+    @classmethod
+    def parse(cls, line):
+        prefix, ch = line.split(':')
+        id_ = int(prefix.split()[1])
+        choices = list(map(Choice.parse, ch.split('; ')))
+        return cls(id_, choices)
+
+COLORS_MAX = {'red': 12, 'green': 13, 'blue': 14}
+print(sum(g.id for g in map(Game.parse, map(str.strip, stdin)) if g.possible(COLORS_MAX)))
---
+++ Day02/2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from sys import stdin
+from collections import Counter
+from functools import reduce
+from operator import mul, or_
+
+class Choice:
+    def __init__(self, colors):
+        self.colors = colors
+
+    @classmethod
+    def parse(cls, choice):
+        colors = Counter()
+        for count, color in map(lambda c: c.split(), choice.split(', ')):
+            colors[color] += int(count)
+        return cls(colors)
+
+
+class Game:
+    def __init__(self, id_, choices):
+        self.id = id_
+        self.choices = choices
+
+    def get_max_color(self):
+        return reduce(or_, (c.colors for c in self.choices))
+
+    def get_power(self):
+        return reduce(mul, self.get_max_color().values())
+
+    @classmethod
+    def parse(cls, line):
+        prefix, ch = line.split(':')
+        id_ = int(prefix.split()[1])
+        choices = list(map(Choice.parse, ch.split('; ')))
+        return cls(id_, choices)
+
+print(sum(map(Game.get_power, map(Game.parse, map(str.strip, stdin)))))
