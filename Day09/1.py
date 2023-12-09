5078d0e8b17b1b95573aa67cccb59688f2102bb5
Jakub Maloštík <j.malostik@gmail.com>
2023-12-09 09:17:14 +0100

    Day 9

---
+++ Day09/1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from sys import stdin
+from itertools import pairwise
+
+
+def sequence_value_derivator(sequence):
+    if (all(v == 0 for v in sequence)):
+        return 0
+    return sequence[-1] + sequence_value_derivator([b - a for a, b in pairwise(sequence)])
+
+
+print(sum(map(sequence_value_derivator, ([int(v) for v in line.split()] for line in stdin))))
