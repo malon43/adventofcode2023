67758f0d248d2cd1aade803ccabc2c150c9d0d6c
Jakub Maloštík <j.malostik@gmail.com>
2023-12-12 12:38:30 +0100

    slight optimization

--- Day12/2.py
+++ Day12/2.py
@@ -10,28 +10,26 @@ MULTIPLIER = 5
 def can_be_placed(springs, i, number):
     return i + number <= len(springs) and all(springs[k] in ('?', '#') for k in range(i, i + number)) and (i + number == len(springs) or springs[i + number] in ('.', '?'))
 
+def solve(springs, numbers):
+    @cache
+    def backtrack(i, j):
+        if j >= len(numbers):
+            return '#' not in springs[i:]
 
-@cache
-def backtrack(springs, numbers, i=0, j=0):
-    if j >= len(numbers):
-        return '#' not in springs[i:]
+        while i < len(springs) and springs[i] == '.':
+            i += 1
 
-    if len(springs) - i < sum(numbers[j:]) + len(numbers) - j - 1:  # Optimization
-        return 0
+        if i >= len(springs):
+            return 0
 
-    while i < len(springs) and springs[i] == '.':
-        i += 1
+        if springs[i] == '#':
+            return backtrack(i + numbers[j] + 1, j + 1) if can_be_placed(springs, i, numbers[j]) else 0
 
-    if i >= len(springs):
-        return 0
-
-    if springs[i] == '#':
-        return backtrack(springs, numbers, i + numbers[j] + 1, j + 1) if can_be_placed(springs, i, numbers[j]) else 0
-
-    res = backtrack(springs, numbers, i + 1, j)
-    if can_be_placed(springs, i, numbers[j]):
-        res += backtrack(springs, numbers, i + numbers[j] + 1, j + 1)
-    return res
+        res = backtrack(i + 1, j)
+        if can_be_placed(springs, i, numbers[j]):
+            res += backtrack(i + numbers[j] + 1, j + 1)
+        return res
+    return backtrack(0, 0)
 
 
-print(sum(starmap(backtrack, (('?'.join((springs,) * MULTIPLIER), tuple([int(i) for i in numbers.split(',')]) * MULTIPLIER) for springs, numbers in map(str.split, map(str.strip, stdin))))))
+print(sum(starmap(solve, (('?'.join((springs,) * MULTIPLIER), [int(i) for i in numbers.split(',')] * MULTIPLIER) for springs, numbers in map(str.split, map(str.strip, stdin))))))
